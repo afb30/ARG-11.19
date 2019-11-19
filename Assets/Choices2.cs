aac3bd905088fbaa57d0d42e12fe07338fdbd419
afb30 <46587212+afb30@users.noreply.github.com>
2019-11-19 15:47:26 -0500

    11.19

---
+++ Assets/Choices2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Choices2 : MonoBehaviour
+{
+public GameObject TextBox;
+public GameObject Choice01;
+public GameObject Choice02;
+public int ChoiceMade;
+
+public void ChoiceOption1 () {
+TextBox.GetComponent<Text>().text = "This could have been much worse without you.";
+ChoiceMade = 1;
+}
+
+public void ChoiceOption2 () {
+TextBox.GetComponent<Text>().text = "Wow. Nice dedication, but wrong answer.";
+ChoiceMade = 2;
+}
+
+    void Update()
+    {
+        if (ChoiceMade >= 1)
+Choice01.SetActive (false);
+Choice02.SetActive (false);
+    }
+}
---
+++ Assets/SteamVR/InteractionSystem/Samples/JoeJeff/Animation/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("space")){
+		anim.SetBool("isFalling",true);
+	}else{
+		anim.SetBool("isFalling",false);
+	}
+    }
+}
